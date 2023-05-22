0b425c6f022233c169ec7d5799c451e7b28ebe4c
Andre <andrepaesbm@gmail.com>
2023-05-22 17:36:13 -0600

    create an about controler to show the About Us content, add the About us route and add the link to the About us page in the top nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  get '/about', to: 'about#show'
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
